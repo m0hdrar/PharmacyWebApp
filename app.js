ad912d527db84de33a8ef429ddd5c1a27cab104a
m0hdrar <mohdrar200@gmail.com>
2023-12-20 00:00:37 +0300

    "added the firebase and changed some of the html and css"

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  remove,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://realtime-database-6131b-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+
 function darkToggle() {
   var element = document.body;
   element.classList.toggle("dark-mode");
