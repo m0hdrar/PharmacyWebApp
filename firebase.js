fd18a29c9acec91eb7b795e0b35ced608d55530c
m0hdrar <mohdrar200@gmail.com>
2023-12-21 00:51:21 +0300

    "added search part "

--- firebase.js
+++ firebase.js
@@ -16,6 +16,8 @@ import {
   getDatabase,
   ref,
   set,
+  get,
+  child,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 const db = getDatabase(app);
 
@@ -24,22 +26,43 @@ let expiry = document.getElementById("medExpiryDate");
 let amount = document.getElementById("medAmount");
 let name = document.getElementById("medName");
 let id = document.getElementById("medId");
-
+let searchBtn = document.getElementById("search-btn");
+let searchResult = document.getElementById("search-result");
 function addData() {
   set(ref(db, "med/" + id.value), {
     medName: name.value,
     medAmount: amount.value,
     medExpiryDate: expiry.value,
   });
-  clearInputFieldEl(expiry)
-  clearInputFieldEl(amount)
-  clearInputFieldEl(name)
-  clearInputFieldEl(id)
+  clearInputFieldEl(expiry);
+  clearInputFieldEl(amount);
+  clearInputFieldEl(name);
+  clearInputFieldEl(id);
 }
+function searchData() {
+  const medRef = ref(db);
 
+  get(child(medRef, "med/" + searchId.value))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        searchResult.innerHTML = `<p> name:  ${snapshot.val().medName} </p>`;
+        searchResult.innerHTML+= `<p> amount:  ${
+          snapshot.val().medAmount
+        } </p>`;
+        searchResult.innerHTML += `<p> expiryDate:  ${
+          snapshot.val().medExpiryDate
+        } </p>`;
+      } else {
+        searchResult.innerHTML = `<p> No data available </p>`;
+      }
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
 saveBtn.addEventListener("click", addData);
-
+searchBtn.addEventListener("click", searchData);
 console.log("firebase.js loaded");
 function clearInputFieldEl(inputFieldEl) {
-  inputFieldEl.value = ""
+  inputFieldEl.value = "";
 }
